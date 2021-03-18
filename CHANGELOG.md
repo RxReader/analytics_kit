de84e085f24e7f27c0b3099fb369735fdd1ea558
v7lin <v7lin@qq.com>
2021-03-18 17:54:57 +0800

    nullsafety

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.1.0-baidu
+
+* nullsafety
+
 ## 0.0.1-baidu
 
 * 初始版本 - 百度
--- lib/src/analytics.dart
+++ lib/src/analytics.dart
@@ -1,24 +1,20 @@
 import 'dart:async';
 import 'dart:io';
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 
 ///
 class Analytics {
   const Analytics._();
 
-  static const MethodChannel _channel =
-      MethodChannel('v7lin.github.io/analytics_kit');
+  static const MethodChannel _channel = MethodChannel('v7lin.github.io/analytics_kit');
 
   ///
   static Future<void> init({
-    @required String appKey,
-    @required String channelId,
+    required String appKey,
+    required String channelId,
     bool debugEnabled = false,
   }) {
-    assert(appKey?.isNotEmpty ?? false);
-    assert(channelId?.isNotEmpty ?? false);
     return _channel.invokeMethod<void>('init', <String, dynamic>{
       'app_key': appKey,
       'channel_id': channelId,
@@ -27,48 +23,53 @@ class Analytics {
   }
 
   ///
-  static Future<void> setOaid({@required String oaid}) {
+  static Future<void> setOaid({
+    required String oaid,
+  }) {
     assert(Platform.isAndroid);
-    assert(oaid?.isNotEmpty ?? false);
     return _channel.invokeMethod('setOaid', <String, dynamic>{
       'oaid': oaid,
     });
   }
 
   ///
-  static Future<void> setAdid({@required String adid}) {
+  static Future<void> setAdid({
+    required String adid,
+  }) {
     assert(Platform.isIOS);
-    assert(adid?.isNotEmpty ?? false);
     return _channel.invokeMethod('setAdid', <String, dynamic>{
       'adid': adid,
     });
   }
 
   ///
-  static Future<void> setAuthorizedState({@required bool enabled}) {
+  static Future<void> setAuthorizedState({
+    required bool enabled,
+  }) {
     assert(Platform.isAndroid);
-    assert(enabled != null);
     return _channel.invokeMethod('setAuthorizedState', <String, dynamic>{
       'enabled': enabled,
     });
   }
 
   ///
-  static Future<String> getTestDeviceId() {
+  static Future<String?> getTestDeviceId() {
     return _channel.invokeMethod<String>('getTestDeviceId');
   }
 
   ///
-  static Future<void> startPageTracking({@required String pageName}) {
-    assert(pageName?.isNotEmpty ?? false);
+  static Future<void> startPageTracking({
+    required String pageName,
+  }) {
     return _channel.invokeMethod<void>('startPageTracking', <String, dynamic>{
       'page_name': pageName,
     });
   }
 
   ///
-  static Future<void> stopPageTracking({@required String pageName}) {
-    assert(pageName?.isNotEmpty ?? false);
+  static Future<void> stopPageTracking({
+    required String pageName,
+  }) {
     return _channel.invokeMethod<void>('stopPageTracking', <String, dynamic>{
       'page_name': pageName,
     });
@@ -76,10 +77,9 @@ class Analytics {
 
   ///
   static Future<void> trackEvent({
-    @required String eventId,
-    Map<String, String> eventParams,
+    required String eventId,
+    Map<String, String>? eventParams,
   }) {
-    assert(eventId?.isNotEmpty ?? false);
     return _channel.invokeMethod<void>('trackEvent', <String, dynamic>{
       'event_id': eventId,
       if (eventParams?.isNotEmpty ?? false) 'event_params': eventParams,
